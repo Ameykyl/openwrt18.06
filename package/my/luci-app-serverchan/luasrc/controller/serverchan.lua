993b61ffd0219e6f7ecc6670cf41aaec7cf6410d
ntlf9t <505575853@qq.com>
2019-07-15 01:13:16 +0800

    mod 3.1.46

--- package/my/luci-app-serverchan/luasrc/controller/serverchan.lua
+++ package/my/luci-app-serverchan/luasrc/controller/serverchan.lua
@@ -1,14 +1,15 @@
-#-- Copyright (C) 2018 dz <dingzhong110@gmail.com>
-
-module("luci.controller.serverchan", package.seeall)
-
+module("luci.controller.serverchan",package.seeall)
 function index()
-	if not nixio.fs.access("/etc/config/serverchan") then
-		return
-	end
+if not nixio.fs.access("/etc/config/serverchan")then
+return
+end
+entry({"admin","services","serverchan"},cbi("serverchan"),_("微信推送"),30).dependent=true
+entry({"admin","services","serverchan","status"},call("act_status")).leaf=true
+end
 
-	local page
-
-	page = entry({"admin", "services", "serverchan"}, cbi("serverchan"), _("serverchan"), 60)
-	page.dependent = true
+function act_status()
+  local e={}
+  e.running=luci.sys.call("pgrep -f serverchan/serverchan >/dev/null")==0
+  luci.http.prepare_content("application/json")
+  luci.http.write_json(e)
 end
