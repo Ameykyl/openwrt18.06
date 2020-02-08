9ccde3efbf0ae0277791266bbedd9586e81a1510
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2020-02-09 00:29:37 +0800

    #478

--- package/my/luci-app-netdata/luasrc/controller/netdata.lua
+++ package/my/luci-app-netdata/luasrc/controller/netdata.lua
@@ -1,9 +1,6 @@
 module("luci.controller.netdata", package.seeall)
 
 function index()
-	if not (luci.sys.call("pidof netdata > /dev/null") == 0) then
-		return
-	end
-	
-	entry({"admin", "system", "netdata"}, template("netdata"), _("NetData"), 10).leaf = true
+
+	entry({"admin","status","netdata"},template("netdata"),_("NetData"),10).leaf=true
 end
