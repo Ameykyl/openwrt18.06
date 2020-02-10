a8afc4e88bcb755dea51a97ffd0e2928af5a4fa1
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2020-02-10 21:53:51 +0800

    #475

--- package/diy/luci-app-rclone/luasrc/controller/rclone.lua
+++
@@ -1,8 +0,0 @@
-module("luci.controller.rclone", package.seeall)
-
-function index()
-	if not nixio.fs.access("/etc/config/rclone") then
-		return
-	end
-	entry({"admin", "services", "rclone"}, cbi("rclone"), _("Rclone"), 100 ).dependent = false
-end
