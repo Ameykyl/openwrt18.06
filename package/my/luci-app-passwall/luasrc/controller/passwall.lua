cda6eaf824fe79099fea9ebb1cbf6d1788f23f93
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2020-02-12 02:18:10 +0800

    passwall : update to 3.5.3 (#223)

--- package/my/luci-app-passwall/luasrc/controller/passwall.lua
+++ package/my/luci-app-passwall/luasrc/controller/passwall.lua
@@ -9,6 +9,7 @@ local v2ray = require "luci.model.cbi.passwall.api.v2ray"
 function index()
     if not nixio.fs.access("/etc/config/passwall") then return end
     entry({"admin", "vpn"}, firstchild(), "VPN", 45).dependent = false
+    entry({"admin", "vpn", "passwall", "reset_config"}, call("reset_config")).leaf = true
     entry({"admin", "vpn", "passwall", "show"}, call("show_menu")).leaf = true
     entry({"admin", "vpn", "passwall", "hide"}, call("hide_menu")).leaf = true
     if nixio.fs.access("/etc/config/passwall") and
@@ -21,10 +22,10 @@ function index()
           _("Basic Settings"), 1).dependent = true
     entry({"admin", "vpn", "passwall", "node_list"}, cbi("passwall/node_list"),
           _("Node List"), 2).dependent = true
-   entry({"admin", "vpn", "passwall", "subscription"},cbi("passwall/subscription"),
+    entry({"admin", "vpn", "passwall", "subscription"},cbi("passwall/subscription"),
            _("Subscription"),3).leaf = true
-    entry({"admin", "vpn", "passwall", "auto_switch"},
-        cbi("passwall/auto_switch"), _("Auto Switch"), 4).leaf = true
+    -- entry({"admin", "vpn", "passwall", "auto_switch"},
+    --      cbi("passwall/auto_switch"), _("Auto Switch"), 4).leaf = true
     entry({"admin", "vpn", "passwall", "other"},
           cbi("passwall/other", {autoapply = true}), _("Other Settings"), 94).leaf =
         true
@@ -39,11 +40,10 @@ function index()
     entry({"admin", "vpn", "passwall", "rule_list"},
           cbi("passwall/rule_list", {autoapply = true}),
           _("Set Blacklist And Whitelist"), 98).leaf = true
-   entry({"admin", "vpn", "passwall", "Status"},form("passwall/Status"),
+    entry({"admin", "vpn", "passwall", "Status"},form("passwall/Status"),
           _("Status"), 99).leaf = true
-    entry({"admin", "vpn", "passwall", "log"},
-          cbi("passwall/log", {autoapply = true}), _("Watch Logs"), 100).leaf =
-        true
+    entry({"admin", "vpn", "passwall", "log"}, cbi("passwall/log"),
+          _("Watch Logs"), 100).leaf = true
     entry({"admin", "vpn", "passwall", "node_config"},
           cbi("passwall/node_config")).leaf = true
 
@@ -82,6 +82,11 @@ end
 local function http_write_json(content)
     http.prepare_content("application/json")
     http.write_json(content or {code = 1})
+end
+
+function reset_config()
+    luci.sys.call('[ -f "/usr/share/passwall/config.default" ] && cp -f /usr/share/passwall/config.default /etc/config/passwall && /etc/init.d/passwall reload')
+    luci.http.redirect(luci.dispatcher.build_url("admin", "vpn", "passwall"))
 end
 
 function show_menu()
@@ -256,7 +261,7 @@ function check_port()
                                         "ms.</font><br />"
                     else
                         retstring = retstring .. "<font color='red'>" ..
-                                        node_name .. "   Error.</font><br />"
+                                        node_name .. "   Timeout.</font><br />"
                     end
                     ret = ""
                 end
